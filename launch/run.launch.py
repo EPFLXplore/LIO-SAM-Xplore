242dab5db145ef0bf3064f93ce7a63c5c0f2622c
margg00 <margueritebenoist@gmail.com>
2024-09-05 04:33:08 +0200

    use lio sam odom in nav2

--- launch/run.launch.py
+++ launch/run.launch.py
@@ -30,7 +30,7 @@ def generate_launch_description():
             # Node(
             #     package='tf2_ros',
             #     executable='static_transform_publisher',
-            #     arguments='0.0 0.0 0.0 0.0 0.0 0.0 map odom'.split(' '),
+            #     arguments='0.0 0.0 0.0 0.0 0.0 0.0 lidar_sensor_frame lidar_link'.split(' '),
             #     parameters=[parameter_file],
             #     output='screen'
             #     ),
